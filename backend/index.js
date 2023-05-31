3db65166c68b2b3d512d54f4441199ea657dd2fa
Mukesh <mouryamukesh8692@gmail.com>
2023-06-01 00:11:16 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      { headers: { "private-key": "076b5b7c-b03e-44a0-baad-db693309ad1d" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+app.listen(3001);
